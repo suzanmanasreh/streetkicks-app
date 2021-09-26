75667a4fcd943861b537e970b74d56662869c0d6
suzm10 <suzanmanasreh@gmail.com>
2021-09-26 15:28:56 -0400

    product setup

---
+++ react-app/src/components/Products/Product/Product.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+const Product = () => {
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Product;

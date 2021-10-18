e9192af7078379f983ab30c52f9ae8beecf0cd71
suzm10 <suzanmanasreh@gmail.com>
2021-10-17 22:00:00 -0400

    add cart functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import Products from "./components/Products/Products";
+import { Products, Navbar } from "./components";
+import { commerce } from "./lib/commerce";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productID, quantity) => {
+    const item = await commerce.cart.add(productID, quantity);
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+  // console.log(products);
+
   return (
     <div>
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
--- react-app/src/components/Products/Products.jsx
+++ react-app/src/components/Products/Products.jsx
@@ -1,33 +1,17 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Adidas Bravadas",
-    description: "Men's sport shoes",
-    price: "$48",
-    image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/e725107a3d7041389f94ab220123fbcb_9366/Bravada_Shoes_Black_FV8085_01_standard.jpg",
-  },
-  {
-    id: 2,
-    name: "Adidas Superstars",
-    description: "Women's basketball shoes",
-    price: "$85",
-    image:
-      "https://assets.adidas.com/images/w_600,f_auto,q_auto/4e894c2b76dd4c8e9013aafc016047af_9366/Superstar_Shoes_White_FV3284_01_standard.jpg",
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>

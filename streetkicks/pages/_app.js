4d194fd0e15c789bc180b9e23a5f2f31424ae7b3
Suzan M <suzanmanasreh@gmail.com>
2021-10-30 17:05:51 -0400

    add products to page

--- streetkicks/pages/_app.js
+++ streetkicks/pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { StoreProvider } from "../components/Store";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />;
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.getInitialProps = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+};

95998f2463c65f8812959a652eaa0f70f3935af3
Suzan M <suzanmanasreh@gmail.com>
2021-10-30 22:02:22 -0400

    got error

--- streetkicks/pages/_app.js
+++ streetkicks/pages/_app.js
@@ -3,15 +3,24 @@ import { StoreProvider } from "../components/Store";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import Router from "next/router";
+import { useEffect } from "react";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <StoreProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </StoreProvider>
   );
 }

d2eeec7b7c94d35b35249d7e5a62a8d03c06afba
Suzan M <suzanmanasreh@gmail.com>
2021-10-31 10:02:40 -0400

    still getting error

--- streetkicks/pages/_document.js
+++ streetkicks/pages/_document.js
@@ -24,11 +24,10 @@ export default class MyDocument extends Document {
 MyDocument.getInitialProps = async (ctx) => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
-  ctx.renderPage = () => {
+  ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
-  };
 
   const initialProps = await Document.getInitialProps(ctx);
   return {
